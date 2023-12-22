050ba86589028438a66a9387cff0880645fff41d
Francisco Filho <francisco@capimgrosso.com>
2023-12-22 09:36:20 -0300

    First commit

---
+++ src/test/java/com/capimgrosso/azure/az204/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.capimgrosso.azure.az204;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
