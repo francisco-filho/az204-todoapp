ea19c639d5f4a53a804f26591e648ed077a2cdb6
Francisco Filho <francisco@capimgrosso.com>
2023-12-22 22:25:27 -0300

    Add CRUD options with CosmosDB

--- src/main/java/com/capimgrosso/azure/az204/TodoApp.java
+++ src/main/java/com/capimgrosso/azure/az204/TodoApp.java
@@ -1,40 +1,83 @@
 package com.capimgrosso.azure.az204;
 
+import com.azure.cosmos.*;
+import com.azure.cosmos.implementation.ImplementationBridgeHelpers;
+import com.azure.cosmos.models.*;
+import com.azure.identity.DefaultAzureCredential;
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import io.smallrye.common.constraint.NotNull;
+import jakarta.annotation.PostConstruct;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 @Path("/tasks")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class TodoApp {
+    private static final Logger LOG = Logger.getLogger(TodoApp.class.getSimpleName());
+    private static final String partitionKey = "name";
+    @ConfigProperty(name = "cosmos.key")
+    String key;
 
     List<Task> taskList = new ArrayList<>();
+    CosmosClient cosmosClient;
+    CosmosContainer container;
 
-    @GET
-    public Response listTasks(){
-        return Response.ok(taskList).build();
+    @PostConstruct
+    private void initCosmosDb(){
+//        var credential = new DefaultAzureCredentialBuilder().build();
+
+        LOG.info("-- Connecting to CosmosDb database ---");
+        cosmosClient = new CosmosClientBuilder()
+                .key(key)
+                .consistencyLevel(ConsistencyLevel.SESSION)
+                .endpoint("https://az204cg.documents.azure.com/")
+                .buildClient();
+        container = getContainer("tasks");
+    }
+
+    private CosmosContainer getContainer(String name){
+        return cosmosClient.getDatabase("TodoApp")
+                .getContainer(name);
     }
 
     @GET
-    @Path("/{id}")
-    public Response getTask(@PathParam("id") @NotNull UUID uuid){
-        for (Task t : taskList) {
-            if (uuid.equals(t.getId())){
-                return Response.ok(t).build();
-            }
-        }
-        return Response.status(404).build();
+    public Response listTasks(){
+        var tasks = getContainer("tasks")
+                .queryItems("select * from c", new CosmosQueryRequestOptions(), Task.class)
+                .stream()
+                .toList();
+        return Response.ok(tasks).build();
+    }
+
+    @GET
+    @Path("/{uuid}")
+    public Response getTask(@PathParam("uuid") @NotNull UUID uuid){
+        var paramList = List.of(new SqlParameter("@id", uuid.toString()));
+        var task = getContainer("tasks")
+                .queryItems(new SqlQuerySpec("select * from c where c.id=@id", paramList),
+                        new CosmosQueryRequestOptions(),
+                        Task.class)
+                .stream()
+                .findFirst()
+                .get();
+
+        return Response.ok(task).build();
     }
 
     @POST
     public Response addTask(Task task){
-        taskList.add(task);
-        return Response.ok().build();
+        var container = getContainer("tasks");
+        container.createItem(task);
+        URI uri = URI.create("http://localhost:8080/tasks/" + task.getId());
+        return Response.created(uri).build();
     }
 }
